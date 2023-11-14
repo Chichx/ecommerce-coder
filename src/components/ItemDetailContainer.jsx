f8706160a1aa5a9745505e81514775f867c1c22b
Chicho <69669676+Chichx@users.noreply.github.com>
2023-11-14 20:49:08 -0300

    Error arreglado

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -29,10 +29,10 @@ return (
     <div className="absolute bottom-0 left-0 w-48 h-48 blur-[250px] bg-gradient-to-r from-orange-300 to-orange-500 rounded-3xl z-[-1]"></div>
     <div  className="relative flex items-center justify-center h-screen gap-64">
 	{item ? <ItemDetail item={item} /> :
-	<div class="flex flex-row gap-2">
-		<div class="w-4 h-4 rounded-full bg-orange-400 animate-bounce"></div>
-		<div class="w-4 h-4 rounded-full bg-orange-400 animate-bounce [animation-delay:-.3s]"></div>
-		<div class="w-4 h-4 rounded-full bg-orange-400 animate-bounce [animation-delay:-.5s]"></div>
+	<div className="flex flex-row gap-2">
+		<div className="w-4 h-4 rounded-full bg-orange-400 animate-bounce"></div>
+		<div className="w-4 h-4 rounded-full bg-orange-400 animate-bounce [animation-delay:-.3s]"></div>
+		<div className="w-4 h-4 rounded-full bg-orange-400 animate-bounce [animation-delay:-.5s]"></div>
 	</div>
 		}
 
